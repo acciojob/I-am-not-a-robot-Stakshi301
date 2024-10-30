4a4750613cf22a11f3506d98ae9b62c23ad640df
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-30 22:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -60,7 +60,7 @@
 
    function verify(){
     if(firstClick && secondClick){
-        if(firstClick.src === secondClick.src){
+        if(firstClick.content === secondClick.content){
             document.getElementById('para').innerHTML = 'Verified';
 
         }else{
